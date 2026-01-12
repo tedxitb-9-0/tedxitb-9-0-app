a608eabf13b73c9a7d99b3b8018632333a772219
Nicholas Wise <nicholasaragih@gmail.com>
2026-01-12 19:49:09 +0700

    refactor: nyusul yah nanti signin dimunculin

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -20,7 +20,7 @@ const LoginPage = () => {
 
   return (
     <>
-     <h1 className="sr-only">TEDxITB 9.0 Login</h1>
+     {/* <h1 className="sr-only">TEDxITB 9.0 Login</h1>
      <ColorfulBackground>
         <motion.div
           animate={{ y: [0, -15, 0] }}
@@ -44,16 +44,17 @@ const LoginPage = () => {
               initial={{ opacity: 0, x: -30 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.6, delay: 0.4 }}
-              className="w-[60%] max-w-xl rounded-xl bg-white p-2 md:p-4 shadow-xl text-center z-40"
+              className="w-[60%] max-w-xl rounded-xl bg-white p-2 md:p-4 shadow-xl text-center z-40 flex justify-center"
         >
-            <button onClick={handleGoogleSignIn} className="text-md md:text-xl p-2 border-black hover:bg-gray-200 rounded-xl flex justify-center items-center gap-2">
+            <button onClick={handleGoogleSignIn} className="text-md md:text-xl p-2 border-black hover:bg-gray-200 rounded-xl flex justify-center items-center gap-2 w-full hover: cursor-pointer">
               
                 <Image src="/google.png" height={50} width={50} alt="Google Logo" className="w-6 md:w-8"/>  
                 Sign In With Google
             </button> 
         </motion.div>
  
-     </ColorfulBackground>
+     </ColorfulBackground> */}
+     <ComingSoon />
     </>
   )
 }
